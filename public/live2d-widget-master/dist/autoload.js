306e15f99c3d0ca46c8f22714fa7c15f79bd334a
chenn114 <2302322040@qq.com>
2025-12-04 17:19:56 +0800

    2
    2

--- public/live2d-widget-master/dist/autoload.js
+++ public/live2d-widget-master/dist/autoload.js
@@ -5,7 +5,7 @@
 
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
-const live2d_path = 'https://fastly.jsdelivr.net/npm/live2d-widgets@1.0.0-rc.6/dist/';
+const live2d_path = 'D:/GithubDesk/GitHubProject/NewPersonalPage/leleo-home-page/public/live2d-widget-master/dist/';
 // const live2d_path = '/dist/';
 
 // Method to encapsulate asynchronous resource loading
